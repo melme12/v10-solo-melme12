e279050ca9559b1622460abffdf6b6ab72563a77
melme12 <32367959+melme12@users.noreply.github.com>
2019-08-13 13:13:53 +0200

    hamburger menu done, fixed some typography stuff

---
+++ script.js
@@ -0,0 +1,14 @@
+//Change Hamburger to X with onclick
+function hamburgerToX(x) {
+  x.classList.toggle("change");
+}
+
+ function showMenu() {
+  var y = document.querySelector('#mainMenu');
+  y.classList.toggle("hide__nav");
+}
+
+// function showMenu(a) {
+//   let g = document.getElementById(a).style.display = "none";
+//   g.classList.toggle("nav__menulist");
+// }
